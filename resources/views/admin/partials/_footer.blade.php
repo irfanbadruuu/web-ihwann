4f8936c41f458ea10ab1f7a684907b455e76edc2
fajar khairudin <kecefajar55@gmail.coml>
2024-02-26 17:16:58 +0700

    first init commit

---
+++ resources/views/admin/partials/_footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="footer text-center text-muted">
+    All Rights Reserved by SMK Al Hafidz Leuwiliang. Designed and Developed by <a href="https://instagram.com/ihwan.mds" target="_blank">IhwaNudin</a>.
+</footer>
